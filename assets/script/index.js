139fa61a4448ebbbb2ef1448c8b0514e09369528
Ali Mahdi <ali.mahdi.engineer@gmail.com>
2025-08-07 14:33:47 +0300

    Update index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,7 @@
+let allSections = document.querySelectorAll(".section");
 
+allSections.forEach((section)=>{
+    section.addEventListener("click", ()=>{
+        window.location.href = section.getAttribute("data-url")
+    });
+});
